3d1e4f0e1dde63a570c208b061788af107dd1da1
Augusto Zanellato <augusto.zanellato@gmail.com>
2021-10-01 12:40:48 +0200

    Add style checks on CI (#1)

--- 20211001/ex2/ex2.py
+++ 20211001/ex2/ex2.py
@@ -1,8 +1,10 @@
-from operator import add, sub, mul, itruediv
+from operator import add, itruediv, mul, sub
 
 ops = [add, sub, mul, itruediv]
 
 a = float(input("Inserisci un numero: "))
 b = float(input("Inserisci un altro numero: "))
-op = int(input("Inserisci un operatore (0 per addizione, 1 per sottrazione, 2 per moltiplicazione oppure 3 per divisione: "))
+op = int(
+    input("Inserisci un operatore (0 per addizione, 1 per sottrazione, 2 per moltiplicazione oppure 3 per divisione: ")
+)
 print(ops[op](a, b))
