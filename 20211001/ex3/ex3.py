184e2b49cad6e64a615ed6c7696d47f2a03f172a
Augusto Zanellato <augustocesare.zanellato@studenti.unipd.it>
2021-10-01 12:32:42 +0200

    Add style checks on CI

--- 20211001/ex3/ex3.py
+++ 20211001/ex3/ex3.py
@@ -1,5 +1,6 @@
 import string
 from random import choices
+
 alphabet = string.ascii_letters + string.digits + string.punctuation
 
 print("".join(choices(alphabet, k=int(input("Inserisci una lunghezza: ")))))
